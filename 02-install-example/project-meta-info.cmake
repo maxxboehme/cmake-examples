c58281e0ed02a3dbec5ceb1c59d4a39cd2c5b02e
maxxboehme <maxxboeme@gmail.com>
2021-08-03 00:15:18 -0500

    Use project-meta-info on all projects and standard project settings

---
+++ 02-install-example/project-meta-info.cmake
@@ -0,0 +1,6 @@
+set(project_version 0.0.1)
+set(project_description "CMake Example Project")
+set(project_homepage "https//github.com/maxxboehme/cmake-examples")
+
+set(project_vendor "Maxx Boehme")
+set(project_contact "maxxboehme")
